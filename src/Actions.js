250a23895cf314f3776004c3d10b574db712814e
themaxhook <bharatchhabra703@gmail.com>
2026-06-19 22:07:21 +0530

    Add Multi-container Docker setup with internal Redis

--- src/Actions.js
+++ src/Actions.js
@@ -4,7 +4,6 @@ const ACTIONS = {
     DISCONNECTED: 'disconnected',
     CODE_CHANGE: 'code-change',
     SYNC_CODE: 'sync-code',
-    LEAVE: 'leave',
     ROOM_STATE: 'room-state',  
 };
 //All your socket event names in one place.
